319e813da713a01ed0481358daac047faf9cc44b
finaritra <finaritraonintsoa1@gmail.com>
2023-05-24 16:02:26 -0500

    user modified

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,6 +13,7 @@ class UserController extends AbstractController
      */
     public function index(): Response
     {
+        $vriable = 'test';
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
